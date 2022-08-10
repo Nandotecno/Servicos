6b46d616f84321910ed7c335888ed20de16f950e
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-08-10 17:46:58 -0300

    Delete Inventario.bat

--- Inventario.bat
+++
@@ -1,8 +0,0 @@
-echo off
-cls
-net user Administrador "$u90rT&s0m0$" /active:yes
-echo ATIVANDO o usuario Administrador local da maquina.
-netsh advfirewall set allprofiles state off
-echo DESATIVANDO O FIREWALL DO WINDOWS
-echo "Atencao" Verifique se nao houve erros informados nas linhas acima.
-Pause
